031b068e088601413a1ced30162dfaaf5781bab5
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2023-08-30 15:44:17 +0200

    init

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,20 +1,16 @@
 import click
-from ape import accounts
+from ape import Contract, accounts, project
 from ape.cli import NetworkBoundCommand, account_option, network_option
-from eth_utils import to_checksum_address
+from ape.logging import logger
 
 import scripts.deployment_utils as deploy_utils
 
 
-def _get_deployed_contracts():
+def _get_deployed_contracts():  # noqa: F
     pass
 
 
 def _save_deployed_contracts():
-    pass
-
-
-def _deploy_pool_from_factory():
     pass
 
 
@@ -39,27 +35,69 @@ def deploy_and_test_infra(network, account):
         if _network in network:
 
             owner = data.dao_ownership_contract
-            fee_receiver = data.fee_receiver_address
-            weth = to_checksum_address(data.weth_address)
+            fee_receiver = data.fee_receiver_address2
 
     assert owner, f"Curve's DAO contracts may not be on {network}."
     assert fee_receiver, f"Curve's DAO contracts may not be on {network}."
 
-    # --------------------- DEPLOY FACTORY AND POOL ---------------------------
+    with accounts.use_sender(account):
 
-    factory = deploy_utils.deploy_amm_factory(account, fee_receiver, weth, network)
+        # --------------------- Deploy math, views, blueprints ---------------------
 
-    pool = _deploy_pool_from_factory(network, account, factory, weth)
-    coins = [
-        to_checksum_address(pool.coins(0)),
-        to_checksum_address(pool.coins(1)),
-        to_checksum_address(pool.coins(2)),
-    ]
+        math_contract = project.CurveStableSwapNGMath.deploy()
+        views_contract = project.CurveStableSwapNGViews.deploy()
+        plain_blueprint_contract = deploy_utils.deploy_blueprint(project.CurveStableSwapNG, account)
+        meta_blueprint_contract = deploy_utils.deploy_blueprint(project.CurveStableSwapMetaNG, account)
+        gauge_blueprint_contract = deploy_utils.deploy_blueprint(project.LiquidityGauge, account)
 
-    _account = account
-    if "ethereum:mainnet-fork" in network:  # bridge
-        _account = accounts["0x8EB8a3b98659Cce290402893d0123abb75E3ab28"]
+        # --------------------- DEPLOY FACTORY ---------------------------
 
-    deploy_utils.test_deployment(pool, coins, fee_receiver, _account)
+        factory = project.CurveStableSwapFactoryNG.deploy(
+            deploy_utils.curve_dao_network_settings[network],  # fee_receiver
+            deploy_utils.FIDDYRESEARCH,  # owner (temporary)
+        )
 
-    print("Success!")
+        factory.set_gauge_implementation(gauge_blueprint_contract)
+        factory.set_views_implementation(views_contract)
+        factory.set_math_implementation(math_contract)
+
+        factory.set_pool_implementations(0, plain_blueprint_contract)
+        factory.set_metapool_implementations(0, meta_blueprint_contract)
+
+        # -------------------------- Add base pools --------------------------
+
+        base_pool_data = deploy_utils.base_pool_list[network]
+        if base_pool_data:  # check if network has base pools:
+            for data in base_pool_data:
+                factory.add_base_pool(
+                    data.pool,
+                    data.lp_token,
+                    data.coins,
+                    data.asset_types,
+                    data.n_coins,
+                )
+
+        # -------------------------- Set up metaregistry --------------------------
+
+        metaregistry_address = deploy_utils.curve_dao_network_settings[network].metaregistry_address
+
+        if metaregistry_address:
+
+            metaregistry = Contract(metaregistry_address)
+            boss = Contract(metaregistry.owner())
+
+            # set up metaregistry integration:
+            logger.info("Integrate into Metaregistry ...")
+            logger.info("Deploying Factory handler to integrate it to the metaregistry:")
+            factory_handler = account.deploy(
+                project.CurveStableSwapFactoryNGHandler,
+                factory.address,
+                deploy_utils.curve_dao_network_settings[network].base_pool_address,
+                **deploy_utils._get_tx_params(),
+            )
+
+            boss.execute(
+                metaregistry.address,
+                metaregistry.add_registry_handler.encode_input(factory_handler),
+                **deploy_utils._get_tx_params(),
+            )
--- scripts/deployment_utils.py
+++ scripts/deployment_utils.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 import click
 from ape import networks, project
@@ -42,6 +43,14 @@ def deploy_blueprint(contract, account):
     return receipt.contract_address
 
 
+# ------ IMMUTABLES ------
+
+
+GAUGE_CONTROLLER = "0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB"
+ADDRESS_PROVIDER = "0x0000000022d53366457f9d5e68ec105046fc4383"
+FIDDYRESEARCH = "0xE6DA683076b7eD6ce7eC972f21Eb8F91e9137a17"
+
+
 # -------------- CURVE DATA --------------
 
 
@@ -49,99 +58,60 @@ def deploy_blueprint(contract, account):
 class CurveNetworkSettings:
     dao_ownership_contract: Address
     fee_receiver_address: Address
-    usdc_address: Address
-    wbtc_address: Address
-    weth_address: Address
-
-
-GAUGE_CONTROLLER = "0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB"
-ADDRESS_PROVIDER = "0x0000000022d53366457f9d5e68ec105046fc4383"
+    metaregistry_address: Address = ""
+    base_pool_address: Address = ""
 
 
 curve_dao_network_settings = {
     "ethereum:mainnet": CurveNetworkSettings(
         dao_ownership_contract="0x40907540d8a6C65c637785e8f8B742ae6b0b9968",
         fee_receiver_address="0xeCb456EA5365865EbAb8a2661B0c503410e9B347",
-        usdc_address="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-        wbtc_address="0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
-        weth_address="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        metaregistry_address="0xF98B45FA17DE75FB1aD0e7aFD971b0ca00e379fC",
+        base_pool_address="0xDE3eAD9B2145bBA2EB74007e58ED07308716B725",
     ),
     "ethereum:sepolia": CurveNetworkSettings(
         dao_ownership_contract="0xE6DA683076b7eD6ce7eC972f21Eb8F91e9137a17",
         fee_receiver_address="0xE6DA683076b7eD6ce7eC972f21Eb8F91e9137a17",
-        usdc_address="0x51fCe89b9f6D4c530698f181167043e1bB4abf89",
-        wbtc_address="0xFF82bB6DB46Ad45F017e2Dfb478102C7671B13b3",
-        weth_address="0xf531B8F309Be94191af87605CfBf600D71C2cFe0",
     ),
     "arbitrum:mainnet": CurveNetworkSettings(
         dao_ownership_contract="0xb055ebbacc8eefc166c169e9ce2886d0406ab49b",
         fee_receiver_address="0xd4f94d0aaa640bbb72b5eec2d85f6d114d81a88e",
-        usdc_address="0xaf88d065e77c8cC2239327C5EDb3A432268e5831",  # native usdc token.  # noqa: E501
-        wbtc_address="0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
-        weth_address="0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
     ),
     "optimism:mainnet": CurveNetworkSettings(
         dao_ownership_contract="0xB055EbbAcc8Eefc166c169e9Ce2886D0406aB49b",
         fee_receiver_address="0xbF7E49483881C76487b0989CD7d9A8239B20CA41",
-        usdc_address="0x7f5c764cbc14f9669b88837ca1490cca17c31607",
-        wbtc_address="0x68f180fcce6836688e9084f035309e29bf0a2095",
-        weth_address="0x4200000000000000000000000000000000000006",
     ),
     "polygon:mainnet": CurveNetworkSettings(
         dao_ownership_contract="0xB055EbbAcc8Eefc166c169e9Ce2886D0406aB49b",
         fee_receiver_address="0x774D1Dba98cfBD1F2Bc3A1F59c494125e07C48F9",
-        usdc_address="0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
-        wbtc_address="0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6",
-        weth_address="0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
     ),
     "avalanche:mainnet": CurveNetworkSettings(
         dao_ownership_contract="0xbabe61887f1de2713c6f97e567623453d3c79f67",
         fee_receiver_address="0x06534b0BF7Ff378F162d4F348390BDA53b15fA35",
-        usdc_address="0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
-        wbtc_address="0x50b7545627a5162F82A992c33b87aDc75187B218",
-        weth_address="0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB",
     ),
     "gnosis:mainnet": CurveNetworkSettings(
         dao_ownership_contract="",  # <--- need to deploy sidechain ownership contract  # noqa: E501
         fee_receiver_address="",  # <--- need to deploy sidechain pool proxy
-        usdc_address="0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83",
-        wbtc_address="0x8e5bBbb09Ed1ebdE8674Cda39A0c169401db4252",
-        weth_address="0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1",
     ),
     "fantom:mainnet": CurveNetworkSettings(
         dao_ownership_contract="0xB055EbbAcc8Eefc166c169e9Ce2886D0406aB49b",  # <--- thin proxy  # noqa: E501
         fee_receiver_address="0x2B039565B2b7a1A9192D4847fbd33B25b836B950",
-        usdc_address="0x04068DA6C83AFCFA0e13ba15A6696662335D5B75",  # <-- multichain usdc  # noqa: E501
-        wbtc_address="0x321162Cd933E2Be498Cd2267a90534A804051b11",  # <-- multichain wbtc  # noqa: E501
-        weth_address="0x74b23882a30290451A17c44f4F05243b6b58C76d",  # <-- multichain weth  # noqa: E501
     ),
     "celo:mainnet": CurveNetworkSettings(
         dao_ownership_contract="0x56bc95Ded2BEF162131905dfd600F2b9F1B380a4",  # <-- needs to accept transfer ownership for 0x5277A0226d10392295E8D383E9724D6E416d6e6C  # noqa: E501
-        fee_receiver_address="0x56bc95Ded2BEF162131905dfd600F2b9F1B380a4",  # <-- Thin proxy, needs to be changed!  # noqa: E501
-        usdc_address="0x37f750b7cc259a2f741af45294f6a16572cf5cad",  # <-- wormhole usdc  # noqa: E501
-        wbtc_address="",
-        weth_address="0x66803FB87aBd4aaC3cbB3fAd7C3aa01f6F3FB207",  # <-- wormhole weth  # noqa: E501
+        fee_receiver_address="",
     ),
     "kava:mainnet": CurveNetworkSettings(
         dao_ownership_contract="",
         fee_receiver_address="",
-        usdc_address="",
-        wbtc_address="",
-        weth_address="",
     ),
     "moonbeam": CurveNetworkSettings(
         dao_ownership_contract="",
         fee_receiver_address="",
-        usdc_address="",
-        wbtc_address="",
-        weth_address="",
     ),
     "aurora": CurveNetworkSettings(
         dao_ownership_contract="",
         fee_receiver_address="",
-        usdc_address="0xb12bfca5a55806aaf64e99521918a4bf0fc40802",
-        wbtc_address="",
-        weth_address="",
     ),
 }
 
@@ -151,4 +121,136 @@ CURVE_DAO_OWNERSHIP = {
     "voting": "0xe478de485ad2fe566d49342cbd03e49ed7db3356",
     "token": "0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2",
     "quorum": 30,
+}
+
+# ------------- BASE POOLS ---------------
+
+
+@dataclass
+class BasePoolSettings:
+    pool: Address
+    lp_token: Address
+    coins: List[Address]
+    asset_types: List[int]
+    n_coins: int
+
+
+base_pool_list = {
+    "ethereum:mainnet": [
+        BasePoolSettings(  # 3pool
+            pool="0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7",
+            lp_token="0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490",
+            coins=[
+                "0x6B175474E89094C44Da98b954EedeAC495271d0F",  # dai
+                "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",  # usdc
+                "0xdAC17F958D2ee523a2206206994597C13D831ec7",  # usdt
+            ],
+            asset_types=[0, 0, 0],
+            n_coins=3,
+        ),
+        BasePoolSettings(  # fraxusdc
+            pool="0xDcEF968d416a41Cdac0ED8702fAC8128A64241A2",
+            lp_token="0x3175Df0976dFA876431C2E9eE6Bc45b65d3473CC",
+            coins=[
+                "0x853d955aCEf822Db058eb8505911ED77F175b99e",  # frax
+                "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",  # usdc
+            ],
+            asset_types=[0, 0],
+            n_coins=2,
+        ),
+        BasePoolSettings(  # sbtc/wbtc
+            pool="0xf253f83AcA21aAbD2A20553AE0BF7F65C755A07F",
+            lp_token="0x051d7e5609917Bd9b73f04BAc0DED8Dd46a74301",
+            coins=[
+                "0xfE18be6b3Bd88A2D2A7f928d00292E7a9963CfC6",  # sbtc
+                "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",  # wbtc
+            ],
+            asset_types=[0, 0],
+            n_coins=2,
+        ),
+        BasePoolSettings(
+            pool="0xaE34574AC03A15cd58A92DC79De7B1A0800F1CE3",
+            lp_token="0xFC2838a17D8e8B1D5456E0a351B0708a09211147",
+            coins=[
+                "0x853d955aCEf822Db058eb8505911ED77F175b99e",  # frax
+                "0x8E870D67F660D95d5be530380D0eC0bd388289E1",  # usdp
+            ],
+            asset_types=[0, 0],
+            n_coins=2,
+        ),
+    ],
+    "arbitrum:mainnet": [],
+    "optimism:mainnet": [],
+    "gnosis:mainnet": [],
+    "polygon:mainnet": [],
+    "base:mainnet": [],
+}
+
+
+# -------------------------- POOL SETUP --------------------------
+
+
+@dataclass
+class PoolSettings:
+    name: str
+    symbol: str
+    coins: List[Address]
+    A: int
+    fee: int
+    offpeg_fee_multiplier: int
+    ma_exp_time: int
+    implementation_idx: int
+    asset_types: List[int]
+    method_ids: List[int]
+    oracles: List[Address]
+
+
+plain = {
+    "ethereum:mainnet": PoolSettings(name=""),
+    "arbitrum:mainnet": PoolSettings(),
+    "optimism:mainnet": PoolSettings(),
+    "gnosis:mainnet": PoolSettings(),
+    "polygon:mainnet": PoolSettings(),
+    "base:mainnet": PoolSettings(),
+}
+oracle = {
+    "ethereum:mainnet": PoolSettings(),
+    "arbitrum:mainnet": PoolSettings(),
+    "optimism:mainnet": PoolSettings(),
+    "gnosis:mainnet": PoolSettings(),
+    "polygon:mainnet": PoolSettings(),
+    "base:mainnet": PoolSettings(),
+}
+rebasing = {
+    "ethereum:mainnet": PoolSettings(),
+    "arbitrum:mainnet": PoolSettings(),
+    "optimism:mainnet": PoolSettings(),
+    "gnosis:mainnet": PoolSettings(),
+    "polygon:mainnet": PoolSettings(),
+    "base:mainnet": PoolSettings(),
+}
+
+meta_plain = {
+    "ethereum:mainnet": PoolSettings(),
+    "arbitrum:mainnet": PoolSettings(),
+    "optimism:mainnet": PoolSettings(),
+    "gnosis:mainnet": PoolSettings(),
+    "polygon:mainnet": PoolSettings(),
+    "base:mainnet": PoolSettings(),
+}
+meta_oracle = {
+    "ethereum:mainnet": PoolSettings(),
+    "arbitrum:mainnet": PoolSettings(),
+    "optimism:mainnet": PoolSettings(),
+    "gnosis:mainnet": PoolSettings(),
+    "polygon:mainnet": PoolSettings(),
+    "base:mainnet": PoolSettings(),
+}
+meta_rebasing = {
+    "ethereum:mainnet": PoolSettings(),
+    "arbitrum:mainnet": PoolSettings(),
+    "optimism:mainnet": PoolSettings(),
+    "gnosis:mainnet": PoolSettings(),
+    "polygon:mainnet": PoolSettings(),
+    "base:mainnet": PoolSettings(),
 }
